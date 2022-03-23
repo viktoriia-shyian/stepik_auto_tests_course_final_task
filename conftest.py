3d327f64d58198d1a6d550392dee3a6c7da7fa0d
viktoriia-shyian <viktoriya.shyyan@gmail.com>
2022-03-24 00:18:10 +0200

    Add conftest.py

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from langcodes.language_lists import CLDR_LANGUAGES
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en", help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser_language = request.config.getoption("language")
+    browser = None
+
+    if browser_language in CLDR_LANGUAGES:
+        print(f"\nset browser language {browser_language}..")
+    else:
+        raise pytest.UsageError("--browser_language should exist in ISO-639-2 language codes")
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", browser_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
