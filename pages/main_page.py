a7577c91a80b5a9d1a015d1eef17061a78eae3eb
viktoriia-shyian <viktoriya.shyyan@gmail.com>
2022-03-25 15:20:16 +0300

    add alert in go_to_login_page in main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
         # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
